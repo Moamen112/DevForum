fd6b35188444465010819c920c89ad8f5b30bc53
Moamen Shehab <65092885+Moamen112@users.noreply.github.com>
2025-09-27 09:58:01 +0300

    Modifying fonts and assets of the projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-import React from "react";
-
-function Home() {
-  return <div>Home</div>;
-}
+const Home = () => {
+  return (
+    <div className="background-light800_dark200 p-8">
+      <h1 className="h2-bold">Tailwind CSS is FUN!!</h1>
+      <p className="font-bold font-space-grotesk">Bold Inter text</p>
+      <p className="font-bold font-inter">Bold Inter text</p>
+      <p className="font-light font-inter">Light Inter text</p>
+      <p className="font-extrabold font-space-grotesk">Extra Bold Inter text</p>
+      <p className="font-extrabold font-inter">Extra Bold Inter text</p>
+    </div>
+  );
+};
 
 export default Home;
