ca276ab8961c214e83208eed7c08b6102ab540ed
Moamen Shehab <65092885+Moamen112@users.noreply.github.com>
2025-09-30 15:59:47 +0300

    MobileNav and NavLinks

---
+++ lib/validations.ts
@@ -0,0 +1,51 @@
+import { z } from "zod";
+
+export const SignInSchema = z.object({
+  email: z
+    .email()
+    .min(1, { message: "Email is required" })
+    .meta({ description: "Please provide a valid email address." }),
+
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters" })
+    .max(100, { message: "Password cannot exceed 100 characters" }),
+});
+
+export const SignUpSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: "Username must be atleast 3 charachters long." })
+    .max(30, { message: "Username cannot exceed 30 characters." })
+    .regex(/^[a-zA-Z0-9_]+$/, {
+      message: "Username can only contain letters, numbers, and underscores.",
+    }),
+
+  name: z
+    .string()
+    .min(1, { message: "Name is required." })
+    .max(50, { message: "Name cannot exceed 50 characters." })
+    .regex(/^[a-zA-Z\s]+$/, {
+      message: "Name can only contain letters and spaces",
+    }),
+
+  email: z
+    .email()
+    .min(1, { message: "Email is required" })
+    .meta({ description: "Please provide a valid email address." }),
+
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters" })
+    .max(100, { message: "Password cannot exceed 100 characters" })
+    .regex(/[A-Z]/, {
+      message: "Password must contain at least on uppercase letter.",
+    })
+    .regex(/[a-z]/, {
+      message: "Password must contain at least on lowercase letter.",
+    })
+    .regex(/[0-9]/, { message: "Password must contain at least on number" })
+    .regex(/[^a-zA-Z0-9]/, {
+      message: "Password must contain at least on special character.",
+    }),
+});
