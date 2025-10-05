d05f4fba8673fa2224a4c8765546ff0e7487c1c5
Moamen Shehab <65092885+Moamen112@users.noreply.github.com>
2025-10-05 20:04:30 +0300

    Setup MongoDB & Mongoose

---
+++ lib/mongoose.ts
@@ -0,0 +1,50 @@
+// getting-started.js
+import mongoose, { Mongoose } from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
+}
+
+interface MongooseCache {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+declare global {
+  var mongoose: MongooseCache;
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+const dbConnect = async (): Promise<Mongoose> => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(MONGODB_URI, {
+        dbName: "dev-forum",
+      })
+      .then((result) => {
+        console.log("MongoDB connected");
+        return result;
+      })
+      .catch((error) => {
+        console.log("MongoDB connection error:", error);
+        throw error;
+      });
+  }
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+};
