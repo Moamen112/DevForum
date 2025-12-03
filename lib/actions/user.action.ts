20123cef452e66b50c8d5d05e120c61b5000c819
Moamen Shehab <65092885+Moamen112@users.noreply.github.com>
2025-12-03 10:02:44 +0200

    Implement getUser Server Action

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -7,10 +7,11 @@ import {
   User as UserType,
 } from "@/types/global";
 import action from "../handlers/actions";
-import { PaginatedSearchSchema } from "../validations";
+import { GetUserSchema, PaginatedSearchSchema } from "../validations";
 import handleError from "../handlers/error";
 import { FilterQuery } from "mongoose";
-import { User } from "@/database";
+import { Answer, Question, User } from "@/database";
+import { GetUserParams } from "@/types/action";
 
 export async function getUsers(
   params: PaginatedSearchParams
@@ -68,6 +69,47 @@ export async function getUsers(
     return {
       success: true,
       data: { users: JSON.parse(JSON.stringify(users)), isNext },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
+
+export async function getUser(params: GetUserParams): Promise<
+  ActionResponse<{
+    user: UserType;
+    totalQuestions: number;
+    totalAnswers: number;
+  }>
+> {
+  const validationResult = await action({
+    params,
+    schema: GetUserSchema,
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { userId } = params;
+
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      throw new Error("User not found.");
+    }
+
+    const totalQuestions = await Question.countDocuments({ author: userId });
+    const totalAnswers = await Answer.countDocuments({ author: userId });
+
+    return {
+      success: true,
+      data: {
+        user: JSON.parse(JSON.stringify(user)),
+        totalQuestions,
+        totalAnswers,
+      },
     };
   } catch (error) {
     return handleError(error) as ErrorResponse;
